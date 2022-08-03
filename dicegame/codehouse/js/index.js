cbe89eed386be233aaeab8f5973956a92e671ec5
NerdyXG <108344292+NerdyXG@users.noreply.github.com>
2022-08-03 15:09:49 +0100

    Update index.js

--- dicegame/codehouse/js/index.js
+++ dicegame/codehouse/js/index.js
@@ -11,6 +11,10 @@
 
 var gender = prompt("Are you male or female?");
 var name = prompt("Enter your name: ");
+var firstLetterOfName = name.slice(0,1);
+var upperCaseFLON = firstLetterOfName.toUpperCase();
+var lowerCaseRON = name.slice(1, name.length).toLowerCase();
+name = upperCaseFLON + lowerCaseRON;
 var dicePath = ["../../images/dice1.png", "../../images/dice2.png", "../../images/dice3.png", "../../images/dice4.png", "../../images/dice5.png", "../../images/dice6.png"]
 
 if (gender.toLowerCase() == "male") {
