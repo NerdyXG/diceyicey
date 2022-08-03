547e99fd1763aa5714c6bd474b4064c957f58b30
NerdyXG <108344292+NerdyXG@users.noreply.github.com>
2022-08-03 13:57:20 +0100

    Add files via upload

---
+++ dicegame/codehouse/js/index.js
@@ -0,0 +1,38 @@
+/*
+
+
+
+
+
+
+
+*/
+
+
+var dicePath = ["../../images/dice1.png", "../../images/dice2.png", "../../images/dice3.png", "../../images/dice4.png", "../../images/dice5.png", "../../images/dice6.png"]
+
+
+function toggleDice() {
+    var playerOne = randomNumberGenerator();
+    var playerTwo = randomNumberGenerator();
+
+    var newImgOne = document.querySelector(".player-one-image");
+    var newImgTwo = document.querySelector(".player-two-image");
+
+    newImgOne.setAttribute("src", dicePath[playerOne]);
+    newImgTwo.setAttribute("src", dicePath[playerTwo]);
+
+    var winner = document.querySelector("#winnerText");
+    if (playerOne < playerTwo) {
+        winner.textContent = "PLAYER TWO WINS!!!";
+    } else if (playerOne > playerTwo) {
+        winner.textContent = "PLAYER ONE WINS!!!";
+    } else {
+        winner.textContent = "IT'S A TIE!!!"
+    }
+}
+
+function randomNumberGenerator() {
+    var randomNumber = Math.floor(Math.random() * 6);
+    return randomNumber;
+}
